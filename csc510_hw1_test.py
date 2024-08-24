63bc4755e2034a4f7f57c2c32740013a605febfd
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-24 01:27:39 +0000

    Added Pytest Workflow

--- csc510_hw1_test.py
+++ csc510_hw1_test.py
@@ -1,10 +1,10 @@
 #for pytest to work, file names should be of the format *_test.py or test_*.py
 import pytest 
-from hw1 import add 
+from csc510_hw1 import add 
 
 def test_passing(): #for pytest to work, function names should be of the format test_*.py
     assert add(2, 3)==5 
     
 def test_failing(): #for pytest to work, function names should be of the format test_*.py
-    assert add(2, "3") == 4
+    assert add(2, 3) == 4
     #pass
