28e91ef193f1940c14e58b0f55e6b9825676d9a1
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-24 01:07:09 +0000

    modified:   hw1_test.py

--- hw1_test.py
+++ hw1_test.py
@@ -1,9 +1,9 @@
 import pytest 
 from hw1 import add 
 
-def passing_test(): #for pytest to work, function names and file names should be of the format *_test.py or test_*.py
+def test_passing(): #for pytest to work, function names and file names should be of the format *_test.py or test_*.py
     assert add(2, 3)==5 
     
-def failing_test(): #for pytest to work, function names and file names should be of the format *_test.py or test_*.py
+def test_failing(): #for pytest to work, function names and file names should be of the format *_test.py or test_*.py
     assert add(2, "3") == 4
     #pass
