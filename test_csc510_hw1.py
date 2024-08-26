526752951caebea00d0d36ae1526fa5e2943d51e
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-26 00:51:43 -0400

    Update test_csc510_hw1.py

--- test_csc510_hw1.py
+++ test_csc510_hw1.py
@@ -1,22 +1,25 @@
 # For pytest to work, file names should be of the format *_test.py or test_*.py
 from csc510_hw1 import SimpleCalculator
 
+# Create an instance of SimpleCalculator
+calculator = SimpleCalculator()
+
 
 def test_addition_correct():
     """Test that the addition function works correctly."""
-    assert SimpleCalculator.add_numbers(2, 3) == 5
+    assert calculator.add_numbers(2, 3) == 5
 
 
 def test_addition_incorrect():
     """Test that the addition function fails with incorrect data."""
-    assert SimpleCalculator.add_numbers(2, 3) == 4
+    assert calculator.add_numbers(2, 3) == 4
 
 
 def test_multiplication_correct():
     """Test that the multiplication function works correctly."""
-    assert SimpleCalculator.multiply_numbers(2, 3) == 6
+    assert calculator.multiply_numbers(2, 3) == 6
 
 
 def test_multiplication_incorrect():
     """Test that the multiplication function fails with incorrect data."""
-    assert SimpleCalculator.multiply_numbers(2, 3) == 4
+    assert calculator.multiply_numbers(2, 3) == 4
