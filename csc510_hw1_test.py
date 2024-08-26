84c99d9821363c922d75a1a9d3a8d41eb795bf35
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-26 00:14:57 -0400

    Update csc510_hw1_test.py

--- csc510_hw1_test.py
+++ csc510_hw1_test.py
@@ -1,10 +1,13 @@
-#for pytest to work, file names should be of the format *_test.py or test_*.py
-import pytest 
-from csc510_hw1 import add 
+# For pytest to work, file names should be of the format *_test.py or test_*.py
+import pytest
+from csc510_hw1 import add_numbers
 
-def test_passing(): #for pytest to work, function names should be of the format test_*.py
-    assert add(2, 3)==5 
-    
-def test_failing(): #for pytest to work, function names should be of the format test_*.py
-    assert add(2, 3) == 4
-    #pass
+
+def test_addition_correct():
+    """Test that the addition function works correctly."""
+    assert add_numbers(2, 3) == 5
+
+
+def test_addition_incorrect():
+    """Test that the addition function fails with incorrect data."""
+    assert add_numbers(2, 3) == 4
