ac7977784695064906db2b528510ed8871fbf02f
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-26 00:48:55 -0400

    Update test_csc510_hw1.py

--- test_csc510_hw1.py
+++ test_csc510_hw1.py
@@ -2,21 +2,21 @@
 from csc510_hw1 import SimpleCalculator
 
 
-def SimpleCalculator.test_addition_correct():
+def test_addition_correct():
     """Test that the addition function works correctly."""
-    assert add_numbers(2, 3) == 5
+    assert SimpleCalculator.add_numbers(2, 3) == 5
 
 
-def SimpleCalculator.test_addition_incorrect():
+def test_addition_incorrect():
     """Test that the addition function fails with incorrect data."""
-    assert add_numbers(2, 3) == 4
+    assert SimpleCalculator.add_numbers(2, 3) == 4
 
 
-def SimpleCalculator.test_multiplication_correct():
+def test_multiplication_correct():
     """Test that the multiplication function works correctly."""
-    assert multiply_numbers(2, 3) == 6
+    assert SimpleCalculator.multiply_numbers(2, 3) == 6
 
 
-def SimpleCalculator.test_multiplication_incorrect():
+def test_multiplication_incorrect():
     """Test that the multiplication function fails with incorrect data."""
-    assert multiply_numbers(2, 3) == 4
+    assert SimpleCalculator.multiply_numbers(2, 3) == 4
