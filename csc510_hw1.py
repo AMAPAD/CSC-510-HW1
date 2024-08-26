d881c2e3b7e226f45fa13739512beada5e3f79e6
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-26 00:35:57 -0400

    Update csc510_hw1.py

--- csc510_hw1.py
+++ csc510_hw1.py
@@ -1,9 +1,22 @@
-def add_numbers(a, b):
-    """Add two numbers and return the result."""
-    return a + b
+class simple_Calculator():
+    def add_numbers(a, b):
+        """Add two numbers and return the result."""
+        return a + b
+    def multiply_numbers(a, b):
+         """Multiply two numbers and return the result."""
+        return a * b
 
 
 num1 = 5
 num2 = 3
-result = add_numbers(num1, num2)
-print("The addition of numbers 5 and 3 is: ", result)
+
+
+calculator_Object = simple_Calculator()
+
+
+addition_Result = calculator_Object.add_numbers(num1, num2)
+print("The addition of numbers 5 and 3 is: ", addition_Result)
+
+
+multiplication_Result = calculator_Object.multiply_numbers(num1, num2)
+print("The multiplication of numbers 5 and 3 is: ", multiplication_Result)
