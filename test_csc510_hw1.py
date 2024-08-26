a51ae8dde6c659088aefba152a1ff4f66fab560c
Akshay Dongare <75126845+Akshay-Dongare@users.noreply.github.com>
2024-08-26 00:16:41 -0400

    Update test_csc510_hw1.py

--- test_csc510_hw1.py
+++ test_csc510_hw1.py
@@ -6,7 +6,6 @@ def test_addition_correct():
     """Test that the addition function works correctly."""
     assert add_numbers(2, 3) == 5
 
-
 def test_addition_incorrect():
     """Test that the addition function fails with incorrect data."""
     assert add_numbers(2, 3) == 4
